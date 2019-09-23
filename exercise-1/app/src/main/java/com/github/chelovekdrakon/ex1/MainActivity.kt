b54398375716d6f6cd6fd6da1f7f2c877739f24e
Fiodar Morau <chelovekdrakon@gmail.com>
2019-09-23 14:58:10 +0300

    ex1/feature: added onClick listener to the MainActivity --> open SecondActivity with TextInput onClick --> open Browser URL

--- exercise-1/app/src/main/java/com/github/chelovekdrakon/ex1/MainActivity.kt
+++ exercise-1/app/src/main/java/com/github/chelovekdrakon/ex1/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.github.chelovekdrakon.ex1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById<TextView>(R.id.title)
+
+        textView.setOnClickListener {
+            openSecondActivity()
+        }
+    }
+
+    private fun openSecondActivity() {
+        val intent: Intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
